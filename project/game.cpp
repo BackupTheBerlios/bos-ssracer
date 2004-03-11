a032d3c7b9eae6bbd6adb9fe7b1f9fbee1f8c69e
meredith <meredith>
2004-03-11 03:15:12 +0000

    *** empty log message ***

--- project/game.cpp
+++ project/game.cpp
@@ -17,7 +17,30 @@ CGame * CGame::GetGamePtr()
 
 void CGame::Initialize()
 {
+	char* filename;
+	char test[10222] = "fjdksjfkaljlsd";
 
+	int i=0;
+	int j=1;
+
+	for(int k=0;k<10;k++) {
+		i++;
+		j += 2;
+	}
+	
+	djsflasjaskdfjs
+		asdfjdkld;asfd
+		asfs
+		afdas
+
+		daf
+		s
+		fa
+		f
+		d
+		fa
+		fa
+		dfa
 
 }
 
