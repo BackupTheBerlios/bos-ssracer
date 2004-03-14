58c58a45400d9476772677ff0c22914f09bb32fe
ramdizzydogg <ramdizzydogg>
2004-03-14 07:54:46 +0000

    More Screens and process inputs in there

--- project/cscreens.cpp
+++ project/cscreens.cpp
@@ -1,8 +1,9 @@
 #include "cscreens.h"
+#include "input.h"
 
 CMainMenu::CMainMenu()
 {
-
+  
   newGame = new CButton();
   newGame->setText("New Game");
   options = new CButton();
@@ -15,6 +16,9 @@ CMainMenu::CMainMenu()
   credits->setText("Credits");
   quit = new CButton();
   quit->setText("Quit");
+  int tempOrder[]={NEW_GAME, OPTIONS, BESTTIMES, HELP, CREDITS, QUIT};
+  memcpy(screenOrder, tempOrder, sizeof(tempOrder));
+  maxScreeni = 5;
 }
 
 
@@ -36,6 +40,27 @@ void CMainMenu::draw()
   quit->draw();
 }
 
+void CMainMenu::processInput(int key)
+{
+  switch(key)
+  {
+  case GAME_TAB:
+  case GAME_DOWN:
+    selectedScreeni++;
+    if (selectedScreeni > maxScreeni)
+    selectedScreeni =0;
+    break;
+  case GAME_UP:
+    selectedScreeni--;
+    if (selectedScreeni < 0)
+      selectedScreeni = maxScreeni;
+    break;
+  case GAME_RETURN:
+  case GAME_NUMPADENTER:
+    gotoScreen = selectedScreeni;
+  }
+}
+
 //New Game Screen
 CNewGame::CNewGame()
 {
@@ -46,6 +71,9 @@ CNewGame::CNewGame()
   name = new CTextField();
   instructions = new CLabel();
   instructions->setText("Please Enter Name:");
+  int tempOrder[]={GARAGE, MAIN_MENU};
+  memcpy(screenOrder, tempOrder, sizeof(tempOrder));
+  maxScreeni = 1;
 
 }
 
@@ -64,8 +92,28 @@ void CNewGame::draw()
   instructions->draw();
 }
 
+void CNewGame::processInput(int key)
+{
+  switch(key)
+  {//TODO Deal With ASCII Chars for TEXTFIELD!!!
+  case GAME_TAB:
+  case GAME_RIGHT:
+    selectedScreeni++;
+    if (selectedScreeni > maxScreeni)
+    selectedScreeni =0;
+    break;
+  case GAME_LEFT:
+    selectedScreeni--;
+    if (selectedScreeni < 0)
+      selectedScreeni = maxScreeni;
+    break;
+  case GAME_RETURN:
+  case GAME_NUMPADENTER:
+    gotoScreen = selectedScreeni;
+  }
+}
 
-CSelectCar::CSelectCar()
+CGarage::CGarage()
 {
   select = new CButton();
   select->setText("Select");
@@ -73,24 +121,50 @@ CSelectCar::CSelectCar()
   cancel->setText("Cancel");
   instructions = new CLabel();
   instructions->setText("Change Cars By Pressing Left/Right");
-
+  int tempOrder[]={HOME, MAIN_MENU};
+  memcpy(screenOrder, tempOrder, sizeof(tempOrder));
+  maxScreeni = 1;
 }
 
-CSelectCar::~CSelectCar()
+CGarage::~CGarage()
 {
   //Do something here
 }
 
-void CSelectCar::draw()
+void CGarage::draw()
 {
   select->draw();
   cancel->draw();
   instructions->draw();
 }
 
+void CGarage::processInput(int key)
+{
+  switch(key)
+  {
+  case GAME_TAB:
+  case GAME_DOWN:
+    selectedScreeni++;
+    if (selectedScreeni > maxScreeni)
+    selectedScreeni =0;
+    break;
+  case GAME_UP:
+    selectedScreeni--;
+    if (selectedScreeni < 0)
+      selectedScreeni = maxScreeni;
+    break;
+  case GAME_LEFT:
+  case GAME_RIGHT:
+//TODO CHANGE 3D Widget CAR  and SELECT CAR IF ENTER
+    break;
+  case GAME_RETURN:
+  case GAME_NUMPADENTER:
+    gotoScreen = selectedScreeni;
+  }
+}
 
-//Win Game Screen
-CWinGame::CWinGame()
+//PostGame Game Screen
+CPostGame::CPostGame()
 {
   onward = new CButton();
   onward->setText("Onward!");
@@ -103,15 +177,21 @@ CWinGame::CWinGame()
   congrats = new CLabel();
   congrats->setText("Congratulations! You Rocked the House");
   placement = new CLabel();;
-  placement->setText("You Placed First!");
+  placement->setText("You Finished the Race in Some Place");
+  winnings = new CLabel();
+  winnings->setText("Good For you buddy, You won a Gazillion Dollars");
+  int tempOrder[]={HOME, IN_GAME, MAIN_MENU};
+  memcpy(screenOrder, tempOrder, sizeof(tempOrder));
+  maxScreeni = 2;
+
 }
 
-CWinGame::~CWinGame()
+CPostGame::~CPostGame()
 {
   //Do something here
 }
 
-void CWinGame::draw()
+void CPostGame::draw()
 {
   onward->draw();
   restartRace->draw();
@@ -121,32 +201,147 @@ void CWinGame::draw()
 
 }
 
-
-//Lose Game Screen
-CLoseGame::CLoseGame()
+void CPostGame::processInput(int key)
 {
+  switch(key)
+  {
+  case GAME_TAB:
+  case GAME_RIGHT:
+    selectedScreeni++;
+    if (selectedScreeni > maxScreeni)
+    selectedScreeni =0;
+    break;
+  case GAME_LEFT:
+    selectedScreeni--;
+    if (selectedScreeni < 0)
+      selectedScreeni = maxScreeni;
+    break;
+  case GAME_UP:
+  case GAME_DOWN:
+    break;
+  case GAME_RETURN:
+  case GAME_NUMPADENTER:
+    gotoScreen = selectedScreeni;
+  }
+}
+
+
+//Pause Game Screen
+CPauseGame::CPauseGame()
+{
+  resume = new CButton();
+  resume->setText("Resume Game");
   restartRace = new CButton();
   restartRace->setText("Restart Race");
   mainMenu = new CButton();
   mainMenu->setText("Back to Main Menu");
 
   //labels
-  loser = new CLabel();
-  loser->setText("You got beat by Miss Daisy!..");
-  placement = new CLabel();;
-  placement->setText("You Placed last");
+  instructions = new CLabel();
+  instructions->setText("Pause Menu");
+  int tempOrder[]={IN_GAME, IN_GAME, MAIN_MENU};
+  memcpy(screenOrder, tempOrder, sizeof(tempOrder));
+  maxScreeni = 2;
 }
 
-CLoseGame::~CLoseGame()
+
+CPauseGame::~CPauseGame()
 {
   //Do something here
 }
 
-void CLoseGame::draw()
+void CPauseGame::draw()
 {
+  resume->draw();
   restartRace->draw();
   mainMenu->draw();
-  loser->draw();
-  placement->draw();
+  instructions->draw();
 
 }
+
+void CPauseGame::processInput(int key)
+{
+  switch(key)
+  {
+  case GAME_TAB:
+  case GAME_DOWN:
+    selectedScreeni++;
+    if (selectedScreeni > maxScreeni)
+    selectedScreeni =0;
+    break;
+  case GAME_UP:
+    selectedScreeni--;
+    if (selectedScreeni < 0)
+      selectedScreeni = maxScreeni;
+    break;
+  case GAME_LEFT:
+  case GAME_RIGHT:
+    break;
+  case GAME_RETURN:
+  case GAME_NUMPADENTER:
+    gotoScreen = selectedScreeni;
+    //TODO DEAL WITH RESTART OR RESUME DEPENDENT ON CHOICE
+  }
+}
+
+//HOME SCREEN
+CHome::CHome()
+{
+  race = new CButton();
+  race->setText("RACE!");
+  performanceUpgrade = new CButton();
+  performanceUpgrade->setText("Tune Your Ride");
+  dealership = new CButton();
+  dealership->setText("Dealership");
+  garage = new CButton();
+  garage->setText("Garage");
+  mainMenu = new CButton();
+  mainMenu->setText("Back to Main Menu");
+
+  //labels
+  home = new CLabel();
+  home->setText("Home");
+  int tempOrder[]={IN_GAME,PERFORMANCE, GARAGE, DEALERSHIP, MAIN_MENU};
+  memcpy(screenOrder, tempOrder, sizeof(tempOrder));
+  maxScreeni = 4;
+}
+
+
+CHome::~CHome()
+{
+  //Do something here
+}
+
+void CHome::draw()
+{
+  race->draw();
+  performanceUpgrade->draw();
+  garage->draw();
+  dealership->draw();
+  mainMenu->draw();
+}
+
+void CHome::processInput(int key)
+{
+  switch(key)
+  {
+  case GAME_TAB:
+  case GAME_DOWN:
+    selectedScreeni++;
+    if (selectedScreeni > maxScreeni)
+    selectedScreeni =0;
+    break;
+  case GAME_UP:
+    selectedScreeni--;
+    if (selectedScreeni < 0)
+      selectedScreeni = maxScreeni;
+    break;
+  case GAME_LEFT:
+  case GAME_RIGHT:
+    break;
+  case GAME_RETURN:
+  case GAME_NUMPADENTER:
+    gotoScreen = selectedScreeni;
+  }
+}
+
--- project/cscreens.h
+++ project/cscreens.h
@@ -5,13 +5,38 @@
 #include "cbutton.h"
 #include "ctextfield.h"
 
+//Hey Gibran, heres my SCREEN MACROS that you can use
+#define MAIN_MENU 1
+#define NEW_GAME 2
+#define GARAGE 3
+#define POST_GAME 4
+#define IN_GAME 5
+#define OPTIONS 6
+#define BESTTIMES 7
+#define HELP 8
+#define CREDITS 9
+#define QUIT 10
+#define PAUSE 11
+#define PERFORMANCE 12
+#define HOME 13
+#define DEALERSHIP 14
+
+
 class CScreen
 {
 public:
   virtual void draw()=0;
+  virtual void processInput(int key)=0;
   //not much else in base class for now
-  CScreen(){}
+  CScreen(){gotoScreen=0; maxScreeni = 1000; selectedScreeni =0;}
   ~CScreen(){}
+
+  CWidget * selectedWidget;
+  int selectedScreeni;
+  int maxScreeni;
+  int gotoScreen;  
+
+
 };
 
 
@@ -26,11 +51,14 @@ class CMainMenu : public CScreen
   CButton * help;
   CButton * credits;
   CButton * quit;
+  int screenOrder[6];
 
   //functions
   CMainMenu();          //constructor where all widgets are set 
   ~CMainMenu();         //destructor
   void draw();          //Jay's stub to render screen
+  void processInput(int key);
+
 
 };
 
@@ -40,7 +68,8 @@ class CNewGame : public CScreen
   public:
     //buttons
   CButton * ok;
-  CButton * cancel;
+  CButton * cancel; 
+  int screenOrder[2];
 
   //labels
   CLabel * instructions;
@@ -52,15 +81,17 @@ class CNewGame : public CScreen
   CNewGame();          //constructor where all widgets are set 
   ~CNewGame();         //destructor
   void draw();          //Jay's stub to render screen
+  void processInput(int key);
 
 };
 
-class CSelectCar : public CScreen
+class CGarage : public CScreen
 {
   public:
     //buttons
   CButton * select;
   CButton * cancel;
+  int screenOrder[2];
 
   //TODO WILL NEED 3D Widget Here to Handle Rotating Car
 
@@ -68,48 +99,78 @@ class CSelectCar : public CScreen
   CLabel * instructions;
 
   //functions
-  CSelectCar();          //constructor where all widgets are set 
-  ~CSelectCar();         //destructor
+  CGarage();          //constructor where all widgets are set 
+  ~CGarage();         //destructor
   void draw();          //Jay's stub to render screen
+  void processInput(int key);
 
 };
 
-class CWinGame : public CScreen
+class CPostGame : public CScreen
 {
   public:
     //buttons
   CButton * onward;
   CButton * restartRace;
   CButton * mainMenu;
-
+  int screenOrder[3];
 
   //labels
   CLabel * congrats;
   CLabel * placement;
+  CLabel * winnings;
 
   //functions
-  CWinGame();          //constructor where all widgets are set 
-  ~CWinGame();         //destructor
+  CPostGame();          //constructor where all widgets are set 
+  ~CPostGame();         //destructor
   void draw();          //Jay's stub to render screen
+  void processInput(int key);
 
 };
 
-class CLoseGame : public CScreen
+class CPauseGame : public CScreen
 {
   public:
     //buttons
+  CButton * resume;
   CButton * restartRace;
   CButton * mainMenu;
-
+  CButton * options;
+  int screenOrder[3];
 
   //labels
-  CLabel * loser;
-  CLabel * placement;
+  CLabel * instructions;
 
   //functions
-  CLoseGame();          //constructor where all widgets are set 
-  ~CLoseGame();         //destructor
+  CPauseGame();          //constructor where all widgets are set 
+  ~CPauseGame();         //destructor
   void draw();          //Jay's stub to render screen
+  void processInput(int key);
 
 };
+
+class CHome : public CScreen
+{
+  public:
+    //buttons
+  CButton * race;
+  CButton * performanceUpgrade;
+  CButton * garage;
+  CButton * dealership;
+  CButton * mainMenu;
+
+  int screenOrder[5];
+
+  //labels
+  CLabel * home;
+
+  //functions
+  CHome();          //constructor where all widgets are set 
+  ~CHome();         //destructor
+  void draw();          //Jay's stub to render screen
+  void processInput(int key);
+
+};
+
+
 #endif
